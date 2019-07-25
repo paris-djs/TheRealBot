d67e84534155b3716ad36d3370eb3fbc7235c6a3
TheRealJan PocketMine Plugins <jan.m.tennert@gmail.com>
2019-07-25 19:18:01 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,25 +5,7 @@ const ad = require("../ad.json")
 
 module.exports = (client, message) => {
    
-if(!ad[message.guild.id]) {
-    ad[message.guild.id] = {
-        c: "Werbung"
-    }
-}
-let adr = ad[message.guild.id].c
-if(!adr) return;
-let adrole = message.guild.roles.find(`name`, `${adr}`);
-if(!adrole) return;
 
-    const swearWordss = ["discord.gg", "https", "http", "www"];
-    if( swearWordss.some(word => message.content.toLowerCase().includes(word)) ) {
-      if(message.author.id === "502923245519896577") return;
-      if(message.member.roles.has(adrole.id)) return;
-      message.delete();
-      message.channel.send(`
-Please don't make advertising, ${message.author}!`).then(msg => {msg.delete(5000)});
-return;
-    }
 
 
 
